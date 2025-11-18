6ba9f19ff212ebcc15fceb52018577234b5e4be8
DontBeFoolish <227914070+DontBeFoolish@users.noreply.github.com>
2025-11-18 12:08:10 -0600

    moved course logic/modules to Course.jsx

--- part2/course-info/src/App.jsx
+++ part2/course-info/src/App.jsx
@@ -1,34 +1,4 @@
-const Header = (props) => <h1>{props.course}</h1>
-
-const Content = ({ parts }) => (
-  <div>
-    {parts.map(part =>
-      <Part key={part.id} part={part}/>
-    )}
-  </div>
-)
-
-const Part = ({ part }) => (
-  <p>
-    {part.name} {part.exercises}
-  </p>
-)
-
-const Total = ({ parts }) => (
-  <b>
-    total exercises - {parts.reduce((sum, part) => {return sum += part.exercises},0)}
-  </b>
-)
-
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header course={course.name}/>
-      <Content parts={course.parts}/>
-      <Total parts={course.parts}/>
-    </div>
-  )
-}
+import Course from './components/Course'
 
 const App = () => {
     const courses = [
