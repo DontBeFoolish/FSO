0986963c7d9fd8aeae03fa9f30b570a10ac5c1fc
DontBeFoolish <227914070+DontBeFoolish@users.noreply.github.com>
2025-11-20 16:30:41 -0600

    express server w/ basic functionality for phonebook

---
+++ part3/phonebookBackend/index.js
@@ -0,0 +1,81 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+let persons = [
+    { 
+      "id": "1",
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.get('/info', (req, res) => {
+  const time = new Date()
+  const total = persons.length
+  res.send(`
+    <div>
+      <p>Phonebook has info for ${total} people</p>
+      <p>${time}</p>
+    </div>
+    `)
+})
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = req.params.id
+  const person = persons.find(p => p.id === id)
+  if (person) {
+    res.json(person)
+  } else {
+    res.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = req.params.id
+  persons = persons.filter(p => p.id !== id)
+  res.status(204).end()
+})
+
+app.post('/api/persons', (req, res) => {
+  const newPerson = req.body
+
+  if (!newPerson.name || !newPerson.number) {
+    return res.status(400).json({ error: 'missing information' })
+  }
+
+  if (persons.some(p => p.number === newPerson.number)) {
+    return res.status(400).json({ error: 'number already exists' })
+  }
+
+  newPerson.id = Math.floor(Math.random() * 10000) + 1
+
+  persons = persons.concat(newPerson)  
+
+  res.json(newPerson)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`${PORT} online`)
+})
