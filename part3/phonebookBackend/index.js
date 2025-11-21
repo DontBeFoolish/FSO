9c08294d4977b08e59af13ea7f86ec6fc4026e40
DontBeFoolish <227914070+DontBeFoolish@users.noreply.github.com>
2025-11-21 14:00:12 -0600

    add .env check for port#

--- part3/phonebookBackend/index.js
+++ part3/phonebookBackend/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 
 morgan.token('body', (req) => JSON.stringify(req.body))
@@ -79,7 +81,7 @@ app.post('/api/persons', (req, res) => {
   res.json(newPerson)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`${PORT} online`)
 })
