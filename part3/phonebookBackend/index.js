61bffb789baa8f08e0fe021bcf740306591e4590
DontBeFoolish <227914070+DontBeFoolish@users.noreply.github.com>
2025-11-26 02:08:34 -0600

    add db get & post functionality

--- part3/phonebookBackend/index.js
+++ part3/phonebookBackend/index.js
@@ -1,5 +1,7 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
+const Person = require('./models/person')
 const app = express()
 
 app.use(express.json())
@@ -7,29 +9,6 @@ app.use(express.static('dist'))
 
 morgan.token('body', (req) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
-
-let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
 
 app.get('/info', (req, res) => {
   const time = new Date()
@@ -42,42 +21,42 @@ app.get('/info', (req, res) => {
     `)
 })
 
-app.get('/api/persons', (req, res) => {
-  res.json(persons)
-})
-
-app.get('/api/persons/:id', (req, res) => {
-  const id = req.params.id
-  const person = persons.find(p => p.id === id)
-  if (person) {
+app.get('/api/people', (req, res) => {
+  Person.find({}).then(person => {
     res.json(person)
-  } else {
-    res.status(404).end()
-  }
+  })
 })
 
-app.delete('/api/persons/:id', (req, res) => {
-  const id = req.params.id
-  persons = persons.filter(p => p.id !== id)
-  res.status(204).end()
+app.get('/api/people/:id', (req, res) => {
+  Person.findById(req.params.id)
+    .then(person => {
+      if (person) {
+        res.json(person)
+      } else {
+        console.log(person)
+        res.status(404).end()
+      }
+    })
+    .catch(error => {
+      console.log(error)
+      res.status(400).send({ error: 'malformatted id' })
+    }) 
 })
 
-app.post('/api/persons', (req, res) => {
-  const newPerson = req.body
-
-  if (!newPerson.name || !newPerson.number) {
+app.post('/api/people', (req, res) => {
+  if (!req.body.name || !req.body.number) {
     return res.status(400).json({ error: 'missing information' })
   }
 
-  if (persons.some(p => p.number === newPerson.number)) {
-    return res.status(400).json({ error: 'number already exists' })
-  }
+  const person = new Person({
+    name: req.body.name,
+    number: req.body.number
+  })
 
-  newPerson.id = Math.floor(Math.random() * 10000) + 1
-
-  persons = persons.concat(newPerson)  
-
-  res.json(newPerson)
+  person.save()
+    .then(savedPerson => {
+      res.json(savedPerson)
+    })  
 })
 
 const PORT = process.env.PORT || 3001
