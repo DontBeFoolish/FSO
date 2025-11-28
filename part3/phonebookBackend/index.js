9aa3b8331d92596d782e43a9fb2845edfe6d83e4
DontBeFoolish <227914070+DontBeFoolish@users.noreply.github.com>
2025-11-28 04:21:10 -0600

    add /info - add more error handling

--- part3/phonebookBackend/index.js
+++ part3/phonebookBackend/index.js
@@ -4,41 +4,47 @@ const morgan = require('morgan')
 const Person = require('./models/person')
 const app = express()
 
-app.use(express.json())
 app.use(express.static('dist'))
+app.use(express.json())
 
 morgan.token('body', (req) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-//app.get('/info', (req, res) => {
-//  const time = new Date()
-//  const total = persons.length
-//  res.send(`
-//    <div>
-//      <p>Phonebook has info for ${total} people</p>
-//      <p>${time}</p>
-//    </div>
-//    `)
-//})
+app.get('/info', (req, res, next) => {
+  const time = new Date()
+  Person.countDocuments({})
+    .then(total => {
+      res.send(
+        `<div>
+          <p>Phonebook has info for ${total} people</p>
+          <p>${time}</p>
+        </div>`
+      )
+    })
+    .catch(error => next(error))
+})
 
-app.get('/api/people', (req, res) => {
-  Person.find({}).then(person => {
-    res.json(person)
-  })
+app.get('/api/people', (req, res, next) => {
+  Person.find({})
+    .then(person => {
+      res.json(person)
+    })
+    .catch(error => next(error))
 })
 
 app.get('/api/people/:id', (req, res, next) => {
-  Person.findById(req.params.id).then(person => {
-    if (person) {
-      res.json(person)
-    } else {
-      res.status(404).end()
-    }
-  })
-  .catch(error => next(error))
+  Person.findById(req.params.id)
+    .then(person => {
+      if (person) {
+        res.json(person)
+      } else {
+        res.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
-app.post('/api/people', (req, res) => {
+app.post('/api/people', (req, res, next) => {
   if (!req.body.name || !req.body.number) {
     return res.status(400).json({ error: 'missing information' })
   }
@@ -48,31 +54,35 @@ app.post('/api/people', (req, res) => {
     number: req.body.number
   })
 
-  person.save().then(savedPerson => {
-      res.json(savedPerson)
-    })  
+  person.save()
+    .then(savedPerson => {
+        res.json(savedPerson)
+      }) 
+    .catch(error => next(error)) 
 })
 
 app.put('/api/people/:id', (req, res, next) => {
-  Person.findById(req.params.id).then(person => {
-    if (!person) {
-      return res.status(404).end()
-    }
+  Person.findById(req.params.id)
+    .then(person => {
+      if (!person) {
+        return res.status(404).end()
+      }
 
-    person.number = req.body.number
-    return person.save().then(updatedPerson => {
-      res.json(updatedPerson)
+      person.number = req.body.number
+      return person.save().then(updatedPerson => {
+        res.json(updatedPerson)
+      })
     })
-  })
-  .catch(error => next(error))
+    .catch(error => next(error))
 })
 
 
 app.delete('/api/people/:id', (req, res, next) => {
-  Person.findByIdAndDelete(req.params.id).then(result => {
-    res.status(204).end()
-  })
-  .catch(error => next(error))
+  Person.findByIdAndDelete(req.params.id)
+    .then(result => {
+      res.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 const unknownEndpoint = (req, res) => {
