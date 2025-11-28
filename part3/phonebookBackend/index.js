527c406f7e851d7783ba3f47c3892b37e427c338
DontBeFoolish <227914070+DontBeFoolish@users.noreply.github.com>
2025-11-28 08:31:05 -0600

    add linting and fixed errors

--- part3/phonebookBackend/index.js
+++ part3/phonebookBackend/index.js
@@ -1,105 +1,106 @@
-require('dotenv').config()
-const express = require('express')
-const morgan = require('morgan')
-const Person = require('./models/person')
-const app = express()
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const Person = require('./models/person');
 
-app.use(express.static('dist'))
-app.use(express.json())
+const app = express();
 
-morgan.token('body', (req) => JSON.stringify(req.body))
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(express.static('dist'));
+app.use(express.json());
+
+morgan.token('body', (req) => JSON.stringify(req.body));
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
 app.get('/info', (req, res, next) => {
-  const time = new Date()
+  const time = new Date();
   Person.countDocuments({})
-    .then(total => {
+    .then((total) => {
       res.send(
         `<div>
           <p>Phonebook has info for ${total} people</p>
           <p>${time}</p>
-        </div>`
-      )
+        </div>`,
+      );
     })
-    .catch(error => next(error))
-})
+    .catch((error) => next(error));
+});
 
 app.get('/api/people', (req, res, next) => {
   Person.find({})
-    .then(person => {
-      res.json(person)
+    .then((person) => {
+      res.json(person);
     })
-    .catch(error => next(error))
-})
+    .catch((error) => next(error));
+});
 
 app.get('/api/people/:id', (req, res, next) => {
   Person.findById(req.params.id)
-    .then(person => {
+    .then((person) => {
       if (person) {
-        res.json(person)
+        res.json(person);
       } else {
-        res.status(404).end()
+        res.status(404).end();
       }
     })
-    .catch(error => next(error))
-})
+    .catch((error) => next(error));
+});
 
 app.post('/api/people', (req, res, next) => {
   const person = new Person({
     name: req.body.name,
-    number: req.body.number
-  })
+    number: req.body.number,
+  });
 
   person.save()
-    .then(savedPerson => {
-        res.json(savedPerson)
-      }) 
-    .catch(error => next(error)) 
-})
+    .then((savedPerson) => {
+      res.json(savedPerson);
+    })
+    .catch((error) => next(error));
+});
 
 app.put('/api/people/:id', (req, res, next) => {
-  Person.findById(req.params.id)
-    .then(person => {
-      if (!person) {
-        return res.status(404).end()
+  const { number } = req.body;
+  Person.findByIdAndUpdate(
+    req.params.id,
+    { number },
+    { new: true, runValidators: true, context: 'query' },
+  )
+    .then((updatedPerson) => {
+      if (updatedPerson) {
+        res.json(updatedPerson);
+      } else {
+        res.status(404).end();
       }
-
-      person.number = req.body.number
-      return person.save().then((updatedPerson) => {
-        res.json(updatedPerson)
-      })
     })
-    .catch(error => next(error))
-})
-
+    .catch((error) => next(error));
+});
 
 app.delete('/api/people/:id', (req, res, next) => {
   Person.findByIdAndDelete(req.params.id)
-    .then(result => {
-      res.status(204).end()
+    .then(() => {
+      res.status(204).end();
     })
-    .catch(error => next(error))
-})
+    .catch((error) => next(error));
+});
 
 const unknownEndpoint = (req, res) => {
-  res.status(404).send({ error: 'unknown endpoint' })
-}
-app.use(unknownEndpoint)
+  res.status(404).send({ error: 'unknown endpoint' });
+};
+app.use(unknownEndpoint);
 
 const errorHandler = (error, req, res, next) => {
-  console.log(error)
-
   if (error.name === 'CastError') {
-    return res.status(400).send({ error: 'malformatted id' })
-  } else if (error.name === 'ValidationError') {
-    return res.status(400).json({ error: error.message })
+    return res.status(400).send({ error: 'malformatted id' });
+  }
+  if (error.name === 'ValidationError') {
+    return res.status(400).json({ error: error.message });
   }
 
-  next(error)
-}
-app.use(errorHandler)
+  return next(error);
+};
+app.use(errorHandler);
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log(`running on ${PORT}`)
-})
+  console.log(`running on ${PORT}`);
+});
--- part3/phonebookBackend/models/person.js
+++ part3/phonebookBackend/models/person.js
@@ -1,42 +1,42 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-const url = process.env.MONGODB_URI
+const url = process.env.MONGODB_URI;
 
-mongoose.set('strictQuery', false)
+mongoose.set('strictQuery', false);
 
-console.log('attempting to connect to db')
+console.log('attempting to connect to db');
 mongoose.connect(url, { family: 4 })
-  .then(result => {
-    console.log('connected to db')
+  .then(() => {
+    console.log('connected to db');
   })
-  .catch(error => {
-    console.log('failed to connect to db', error)
-  })
+  .catch((error) => {
+    console.log('failed to connect to db', error);
+  });
 
 const personSchema = mongoose.Schema({
   name: {
     type: String,
     minLength: 3,
-    required: true
+    required: true,
   },
   number: {
     type: String,
     minLength: 8,
     validate: {
-      validator: (v) => {
-        return /^\d{2,3}-\d+$/.test(v)
-      }
+      validator: (v) => /^\d{2,3}-\d+$/.test(v),
     },
-    required: true
-  }
-})
+    required: true,
+  },
+});
 
+/* eslint-disable no-param-reassign, no-underscore-dangle */
 personSchema.set('toJSON', {
   transform: (document, returnedObj) => {
-    returnedObj.id = returnedObj._id.toString()
-    delete returnedObj._id
-    delete returnedObj.__v
-  }
-})
+    returnedObj.id = returnedObj._id.toString();
+    delete returnedObj._id;
+    delete returnedObj.__v;
+  },
+});
+/* eslint-disable no-param-reassign, no-underscore-dangle */
 
-module.exports = mongoose.model('Person', personSchema)
+module.exports = mongoose.model('Person', personSchema);
